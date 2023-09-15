d77e59a9b7464c0d36a630f9af6ec244b5a0d5f3
Vinicch <vinicch.leme@gmail.com>
2023-09-15 03:03:31 -0300

    initial

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use std::{process::Command, thread, time};
+
+fn main() {
+    let mut universe = generate_universe(20, 20);
+    let generations = 999;
+    for _ in 0..=generations {
+        thread::sleep(time::Duration::from_millis(500));
+
+        clear_terminal();
+
+        for row in &universe {
+            println!("{:?}", row);
+        }
+
+        universe = game_of_life(universe);
+    }
+}
+
+fn generate_universe(rows: usize, cols: usize) -> Vec<Vec<u8>> {
+    let mut universe = vec![vec![0_u8; cols]; rows];
+
+    for row in 0..rows {
+        for col in 0..cols {
+            if rand::random() {
+                universe[row][col] = 1;
+            }
+        }
+    }
+
+    universe
+}
+
+fn game_of_life(current_gen: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+    let rows = current_gen.len();
+    let cols = current_gen[0].len();
+
+    let mut next_gen = vec![vec![0_u8; cols]; rows];
+
+    for row in 0..rows {
+        for col in 0..cols {
+            let current_cell = current_gen[row][col];
+            let mut live_neighbors = 0;
+
+            // count live neighbors
+            for x in -1i8..=1 {
+                for y in -1i8..=1 {
+                    let neighbor_row = (row as i8) + x;
+                    let neighbor_col = (col as i8) + y;
+
+                    if neighbor_row > 0
+                        && neighbor_col > 0
+                        && neighbor_row < rows as i8
+                        && neighbor_col < cols as i8
+                    {
+                        live_neighbors += current_gen[neighbor_row as usize][neighbor_col as usize];
+                    }
+                }
+            }
+
+            if live_neighbors > 0 {
+                live_neighbors -= current_cell;
+            }
+
+            // apply rules
+            next_gen[row][col] = if current_cell == 1 && !(2..=3).contains(&live_neighbors) {
+                0
+            } else if current_cell == 0 && live_neighbors == 3 {
+                1
+            } else {
+                current_cell
+            }
+        }
+    }
+
+    next_gen
+}
+
+fn clear_terminal() {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd").args(["/C", "cls"]).status().unwrap();
+        return;
+    }
+
+    Command::new("clear").status().unwrap();
+}
